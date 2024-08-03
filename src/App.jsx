6796560a3ea961079aa3b07301bec190ce8a43eb
Juan Pablo Ferro Arroyo <juanpabloferroarroyo@gmail.com>
2024-08-03 10:32:53 -0400

    convert to 30fit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import logo from "./assets/logo.svg"
 
 function App() {
-  return (
-    <>
-      <img className="logo" src={logo} alt=""/>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
