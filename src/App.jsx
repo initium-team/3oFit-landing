5415fd802f7e821c16e6f4c3690a3fd535f59594
Juan Pablo Ferro Arroyo <juanpabloferroarroyo@gmail.com>
2024-08-12 17:03:56 -0400

    update landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <section className="main-section">
+      <img src="./logo.svg" id="logo-img" />
+
+      <div className="content-container">
+        <div className="bottles-container">
+          <img src="./public/bottles.png" />
+        </div>
+        <div className="text-container">
+          <img src="./public/texts.png" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
